f462d71e94d208deb88cb65d86cc8f44f3e805db
Hasaan Gilani <gilani.4@gmail.com>
2017-03-31 15:43:18 +0500

    fixed migration naming

--- database/migrations/2017_03_04_165202_update_existing_relationships.php
+++ database/migrations/2017_03_04_165202_update_existing_relationships.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class UpdateDatabaseRelationships extends Migration {
+class UpdateExistingRelationships extends Migration {
 	/**
 	 * Run the migrations.
 	 *

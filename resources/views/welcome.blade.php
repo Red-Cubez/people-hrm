2663b3e41ac0b9db7ea9214fa208cb4de3a0d075
Hasaan Gilani <gilani.4@gmail.com>
2017-03-31 11:04:51 +0500

    some simple ui changes to clean up the page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,18 +64,7 @@
             {{--}--}}
         {{--</style>--}}
     {{--</head>--}}
-    {{--<body>--}}
-        {{--<div class="flex-center position-ref full-height">--}}
-            {{--@if (Route::has('login'))--}}
-                {{--<div class="top-right links">--}}
-                    {{--@if (Auth::check())--}}
-                        {{--<a href="{{ url('/home') }}">Home</a>--}}
-                    {{--@else--}}
-                        {{--<a href="{{ url('/login') }}">Login</a>--}}
-                        {{--<a href="{{ url('/register') }}">Register</a>--}}
-                    {{--@endif--}}
-                {{--</div>--}}
-            {{--@endif--}}
+
 
 
 @extends('layouts.master')
@@ -87,14 +76,8 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">Employees</a>
+                    <a href="https://laracasts.com">Companies</a>
+                    <a href="https://laravel-news.com">Projects</a>
                 </div>
                     @endsection
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</body>--}}
-{{--</html>--}}

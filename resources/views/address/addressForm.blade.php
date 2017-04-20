569b6987ae47f51af364f61d7c8982dfcdf2d596
Usman Javed <usman.4601@gmail.com>
2017-04-20 17:38:54 +0500

    add addresses to clients

--- resources/views/address/addressForm.blade.php
+++ resources/views/address/addressForm.blade.php
@@ -1,29 +1,29 @@
   <div class="form-group">
         <label for="contactPerson" class="col-sm-3 control-label">Street Line 1</label>
             <div class="col-sm-6">
-                <input type="text" name="streetLine1" id="streetLine1" class="form-control" @if(isset($clientAddress))
-                 value="{{$client->contactPerson}}" @else placeholder="Enter Street Line 1" @endif >
+                <input type="text" name="streetLine1" id="streetLine1" class="form-control" @if(isset($client))
+                 value="{{$client->address->streetLine1 }}" @else placeholder="Enter Street Line 1" @endif >
             </div>
  </div>
  <div class="form-group">
         <label for="contactPerson" class="col-sm-3 control-label">Street Line 2</label>
             <div class="col-sm-6">
                 <input type="text" name="streetLine2" id="streetLine2" class="form-control" @if(isset($client))
-                 value="{{$client->contactPerson}}" @else placeholder="Enter Street Line 2" @endif >
+                 value="{{$client->address->streetLine2}}" @else placeholder="Enter Street Line 2" @endif >
             </div>
  </div>
   <div class="form-group">
         <label for="contactPerson" class="col-sm-3 control-label">Country</label>
             <div class="col-sm-6">
                 <input type="text" name="country" id="country" class="form-control" @if(isset($client))
-                 value="{{$client->contactPerson}}" @else placeholder="Enter Country" @endif  >
+                 value="{{$client->address->country}}" @else placeholder="Enter Country" @endif  >
             </div>
  </div>
  <div class="form-group">
         <label for="contactPerson" class="col-sm-3 control-label">State / Province</label>
             <div class="col-sm-6">
                 <input type="text" name="stateProvince" id="stateProvince" class="form-control" @if(isset($client))
-                 value="{{$client->contactPerson}}" @else placeholder="State / Province" @endif >
+                 value="{{$client->address->stateProvince}}" @else placeholder="State / Province" @endif >
             </div>
  </div>
 
@@ -31,7 +31,7 @@
         <label for="contactPerson" class="col-sm-3 control-label">City</label>
             <div class="col-sm-6">
                 <input type="text" name="city" id="city" class="form-control" @if(isset($client))
-                 value="{{$client->city}}" @else placeholder="City" @endif >
+                 value="{{$client->address->city}}" @else placeholder="City" @endif >
             </div>
  </div>
 

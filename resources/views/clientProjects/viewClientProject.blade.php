3d05199d76f08d6a47fef1ba885f156fdbc01ed5
Usman Javed <usman.4601@gmail.com>
2017-07-11 12:37:40 +0500

    validation completed and fixed page refresh problem

--- resources/views/clientProjects/viewClientProject.blade.php
+++ resources/views/clientProjects/viewClientProject.blade.php
@@ -5,15 +5,16 @@
         <div class="row">
             <div class="col-sm-4">
                 @include('viewProject/viewProject')
+                <a href="/clientprojects/{{$project->projectId}}/edit">
+                    <button class="btn btn-primary"> Edit</button></a>
+                {{--<form action="{{ url('clientprojects/'.$project->projectId.'/edit') }}" method="POST">--}}
+                    {{--{{ csrf_field() }}--}}
+                    {{--{{ method_field('GET') }}--}}
 
-                <form action="{{ url('clientprojects/'.$project->projectId.'/edit') }}" method="POST">
-                    {{ csrf_field() }}
-                    {{ method_field('GET') }}
-
-                    <button type="submit" class="btn btn-primary">
-                        <i class="fa fa-trash"> Edit</i>
-                    </button>
-                </form>
+                    {{--<button type="submit" class="btn btn-primary">--}}
+                        {{--<i class="fa fa-trash"> Edit</i>--}}
+                    {{--</button>--}}
+                {{--</form>--}}
             </div>
             <div class="col-sm-8">
                 @include('showGraph/showProjectGraph')

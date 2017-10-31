3cdd8c66e983e40fcca586ee0f0e23ef103c138f
Usman Javed <usman.4601@gmail.com>
2017-10-31 13:29:47 +0500

    reports completed with deletion of extra libraries v4.1

--- resources/views/reports/allProjectsGraphs/showProjectsGraphs.blade.php
+++ resources/views/reports/allProjectsGraphs/showProjectsGraphs.blade.php
@@ -102,13 +102,7 @@ No Record Found
 @endsection
 @section('page-scripts')
     @parent
-<!-- Load c3.css -->
-<link href="/css/c3.css" rel="stylesheet">
-    <!-- Load d3.js and c3.js -->
-    <script charset="utf-8" src="/js/d3.v3.min.js">
-    </script>
-    <script src="/js/c3.min.js">
-    </script>
+
     <script src="https://www.gstatic.com/charts/loader.js" type="text/javascript">
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.js">
@@ -120,4 +114,3 @@ No Record Found
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js">
     </script>
     @endsection
-</link>
--- resources/views/reports/showProjectsGraphs.blade.php
+++ resources/views/reports/showProjectsGraphs.blade.php
@@ -62,12 +62,7 @@ No Record Found
 @endsection
 @section('page-scripts')
     @parent
-    <!-- Load c3.css -->
-    <link href="/css/c3.css" rel="stylesheet">
-
-    <!-- Load d3.js and c3.js -->
-    <script src="/js/d3.v3.min.js" charset="utf-8"></script>
-    <script src="/js/c3.min.js"></script>
+    
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.min.js"></script>
